66e3957f1bfc6489b8ee3aacddf85bccd77bc7e8
Artemii <latovgor@gmail.com>
2025-07-03 00:11:25 +0300

    ThemeLocalStorage

--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -3,13 +3,22 @@ import styles from "./Header.module.scss"
 import { useSelector, useDispatch } from 'react-redux';
 import { toggleTheme } from 'shared/model/themeSlice';
 import { useEffect } from "react";
+import { setTheme } from 'shared/model/themeSlice';
 
 export function Header() {
   const theme = useSelector((state: any) => state.theme.theme);
   const dispatch = useDispatch();
   
   useEffect(() => {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme && savedTheme !== theme) {
+      dispatch(setTheme(savedTheme));
+    }
+  }, []);
+
+  useEffect(() => {
     document.body.className = theme; // или добавь/удали класс
+    localStorage.setItem('theme', theme); // сохраняем тему
   }, [theme]);
 
   return (
